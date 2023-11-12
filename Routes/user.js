e6fbf6f93de2590f63206a40095424620cac52dd
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2023-11-12 22:26:24 +0530

    add read,updte & delete

--- Routes/user.js
+++ Routes/user.js
@@ -2,18 +2,72 @@ const express = require("express");
 const router = express.Router();
 const User = require("../Models/user");
 
+//create users
 router.post("/users", async (req, res) => {
-  const user = new User({
-    name: "test",
-    email: "test@example.com",
-    age: 25,
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.status(201).send(user);
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+//read users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    users.status(200).send(users);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+//get user ID
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findById(_id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+//update user
+router.patch("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+    });
+    if (!updatedUser) {
+      return res.status(404).send();
+    }
+    res.status(200).send(updatedUser);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+//delete user
+router.delete("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const deleteUser = await User.findByIdAndDelete(_id);
+    if (!deleteUser) {
+      return res.status(404).send();
+    }
+    res.status(200).send(deleteUser);
+  } catch (error) {
+    res.status(404).send(error);
   }
 });
 
--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.use(taskRouter);
 
 const port = 3000;
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is listening on port " + port);
 });
